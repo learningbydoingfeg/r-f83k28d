b21721359648265733e7799ac02d44df58023ff5
AsusOnePlus X <onex@AsusOnePlusX.local>
2020-08-11 16:03:01 -0500

    solving exercise

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    increment: 0
+  }
+    
+  
+
+handleClick = () => {
+  this.setState({
+    increment: this.state.increment + 1 
+  })
+
+}  
+
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.increment}</span>
+        <button onClick={this.handleClick} id="inc">Incrementa</button>
       </div>
     );
   }
